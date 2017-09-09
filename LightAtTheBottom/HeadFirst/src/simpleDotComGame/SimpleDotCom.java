77e3a94f87d4a9418c1c843d2375780093774512
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2017-09-09 21:44:32 +0200

    theres a frikin bug there damn xd

--- LightAtTheBottom/HeadFirst/src/simpleDotComGame/SimpleDotCom.java
+++ LightAtTheBottom/HeadFirst/src/simpleDotComGame/SimpleDotCom.java
@@ -97,4 +97,31 @@ public class SimpleDotCom
 		
 	}
 	
+	public static void main(String[] args)
+	{
+		int numOfGuesses = 0;
+		
+		GameHelper helper = new GameHelper(); // wtf class with user input capabilities
+		SimpleDotCom theDotCom = new SimpleDotCom(); // create dot com ship
+		
+		int randomNum = (int) ( Math.random() * 5);
+		int[] locations = {randomNum, randomNum+1, randomNum+2};
+		theDotCom.setLocationCells(locations);
+		boolean isAlive = true;
+		
+		while (isAlive == true)
+		{
+			String guess = helper.getUserInput("Enter a number");
+			String result = theDotCom.checkYourself(guess);
+			numOfGuesses++;
+			if (result.equals("kill"))
+			{
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + " guesses.");
+			}
+		}
+		
+		
+	}
+	
 }
