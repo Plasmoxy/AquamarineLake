40ceffd1ab90744241881331763426eaab500231
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2017-09-14 22:10:36 +0200

    added output and unsorted

--- LightAtTheBottom/Laboratory/src/alpha/ArrayListTest.java
+++ LightAtTheBottom/Laboratory/src/alpha/ArrayListTest.java
@@ -16,6 +16,8 @@ public class ArrayListTest
 		ArrayList<Dog> dogs = new ArrayList<Dog>();
 		ArrayList<Cat> cats = new ArrayList<Cat>();
 		
+		ArrayList unsorted = new ArrayList();
+		
 		// add some dogs and cats to trash and also some junk
 		trash.addAll(Arrays.asList(
 			new Dog(1), new Dog(3), "TF", new Cat(4), new Cat(5),
@@ -31,17 +33,21 @@ public class ArrayListTest
 		// sort dogs and cats from trash to their own lists
 		for (Object i : trash)
 		{
-			if ( i instanceof Dog) dogs.add((Dog)i);
-			if ( i instanceof Cat) cats.add((Cat)i);
+				 if ( i instanceof Dog) dogs.add((Dog)i);
+			else if ( i instanceof Cat) cats.add((Cat)i);
+			else 						unsorted.add(i);
 		}
 		
-		// print dogs and cats simultaneously
+		// print dogs and cats simultaneously and also unsorted stuff
 		
 		System.out.println(" Dogs -------------------- \n");
 		for (Dog d : dogs) d.dump();
 		
 		System.out.println(" Cats -------------------- \n");
 		for (Cat c : cats) c.dump();
+		
+		System.out.println(" Unsorted junk -------------------- \n");
+		for (Object o : unsorted) System.out.println(o.toString());
 		
 	}
 	
