f6789782f018f78a81d472f0e6a5e2e1a21f04c7
Plasmoxy <sebohacker@gmail.com>
2017-10-02 22:17:25 +0200

    pmxymididel

--- LightAtTheBottom/HeadFirst/src/pmxymidi/NoteParseException.java
+++
@@ -1,9 +0,0 @@
-package pmxymidi;
-
-public class NoteParseException extends Exception
-{
-	public NoteParseException()
-	{
-		super("NoteParser : The note syntax is incorrect ! Can't parse. ");
-	}
-}
