d4756da146e501c9687d8dca5afb1a4351dd2526
Plasmoxy <sebohacker@gmail.com>
2017-10-22 15:53:26 +0200

    opemncvvs

--- LightAtTheBottom/opencvTest/src/test1/Test1.java
+++ LightAtTheBottom/opencvTest/src/test1/Test1.java
@@ -1,13 +1,22 @@
 package test1;
 
-import org.opencv.core.*;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.*;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Scalar;
 
-public class Test1
-{
-	public static void main(String[] args)
-	{
-		
+public class Test1 {
+	// Load dll
+	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to OpenCV " + Core.VERSION);
+		Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+		System.out.println("OpenCV Mat: " + m);
+		Mat mr1 = m.row(1);
+		mr1.setTo(new Scalar(1));
+		Mat mc5 = m.col(5);
+		mc5.setTo(new Scalar(5));
+		System.out.println("OpenCV Mat data:\n" + m.dump());
 	}
 }
