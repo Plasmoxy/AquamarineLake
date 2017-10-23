7b116889e98c1b85a4b5436d31e5f101149dd9d9
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2017-10-23 16:02:34 +0200

    reeeeeee

---
+++ DifferentUniverse/SeeingTestAlpha/src/DrawPanel.java
@@ -0,0 +1,43 @@
+import org.opencv.core.Mat;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+
+public class DrawPanel extends JPanel {
+
+    private volatile Mat frame;
+
+    public DrawPanel()
+    {
+
+    }
+
+    public void setFrame(Mat m)
+    {
+        frame = m;
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.drawImage(bufferedImage(frame), 0, 0, null);
+    }
+
+    public BufferedImage bufferedImage(Mat m) {
+
+
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if (m.channels() > 1) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
+
+        m.get(0, 0, ((DataBufferByte)image.getRaster().getDataBuffer()).getData());
+        return image;
+
+    }
+
+}
---
+++ DifferentUniverse/SeeingTestAlpha/src/MainFrame.java
@@ -0,0 +1,58 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MainFrame extends JFrame {
+
+    public volatile VideoCapture cap;
+    public volatile DrawPanel p;
+    public static volatile MainFrame window;
+
+    static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
+
+    public MainFrame()
+    {
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        setPreferredSize(new Dimension(640, 480));
+
+        cap = new VideoCapture(0);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                cap.release();
+                System.exit(0);
+            }
+        });
+
+        p = new DrawPanel();
+        add(p);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+
+        window = new MainFrame();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Mat frame = new Mat();
+                while (true)
+                {
+                    window.cap.read(frame);
+                    window.p.repaint();
+                }
+            }
+        }).start();
+    }
+
+}
