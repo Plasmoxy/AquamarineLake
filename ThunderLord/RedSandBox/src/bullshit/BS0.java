f877f3a075ce6f20f32e3962919c6388694c1a27
Plasmoxy <sebohacker@gmail.com>
2017-12-16 22:41:55 +0100

    ikkkkdkk

--- ThunderLord/RedSandBox/src/bullshit/BS0.java
+++ ThunderLord/RedSandBox/src/bullshit/BS0.java
@@ -2,7 +2,6 @@ package bullshit;
 
 public class BS0
 {
-    
     public static void main(String[] args) throws Exception
     {
         TestMention m = new TestMention();
---
+++ ThunderLord/RedSandBox/src/bullshit/idk.java
@@ -0,0 +1,9 @@
+package bullshit;
+
+public class idk
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
