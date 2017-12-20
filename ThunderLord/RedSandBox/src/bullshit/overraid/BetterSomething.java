f29c5b5f544621caf7019c9bb6f4d7c40649dad4
Plasmoxy <sebohacker@gmail.com>
2017-12-20 11:06:02 +0100

    smth :aerial_tramway:

---
+++ ThunderLord/RedSandBox/src/bullshit/overraid/BetterSomething.java
@@ -0,0 +1,15 @@
+package bullshit.overraid;
+
+public class BetterSomething extends Something
+{
+    @Override
+    public void go()
+    {
+        System.out.println("LOLOLO 2");
+    }
+    
+    public BetterSomething()
+    {
+        
+    }
+}
---
+++ ThunderLord/RedSandBox/src/bullshit/overraid/Something.java
@@ -0,0 +1,13 @@
+package bullshit.overraid;
+
+public class Something
+{
+    public void go()
+    {
+        System.out.println("LOL 1 ");
+    }
+    public Something()
+    {
+        
+    }
+}
