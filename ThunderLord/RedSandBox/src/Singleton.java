ba1407dc110f62d7716d58659198734cff26e9d1
Plasmoxy <sebohacker@gmail.com>
2017-12-28 23:05:12 +0100

    singlk

---
+++ ThunderLord/RedSandBox/src/Singleton.java
@@ -0,0 +1,10 @@
+public class Singleton {
+	private static Singleton ourInstance = new Singleton();
+
+	public static Singleton getInstance() {
+		return ourInstance;
+	}
+
+	private Singleton() {
+	}
+}
